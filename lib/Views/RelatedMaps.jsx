718dfbcad4a45b0c7df9664705c00157e8ac8360
Joerg Roth <info@leylines.ch>
2016-07-08 10:32:51 +0200

    fixes for newui

--- lib/Views/RelatedMaps.jsx
+++ lib/Views/RelatedMaps.jsx
@@ -24,65 +24,16 @@ function RelatedMaps(props) {
             </p>
 
             <div className={classNames(DropdownStyles.section, Styles.section)}>
-                <a target="_blank" href="http://nationalmap.gov.au/renewables/">
-                    <img className={Styles.image} src={require('../../wwwroot/images/aremi.jpg')}
-                         alt="AREMI"/>
+                <a target="_blank" href="http://megalithic.co.uk/">
+                    <img className={Styles.image} src={require('../../wwwroot/images/megalithic/megalithic_logo_150.gif')}
+                         width="150" height="150" alt="megalithic.co.uk"/>
                 </a>
 
-                <a target="_blank" className={Styles.link} href="http://nationalmap.gov.au/renewables/">AREMI</a>
+                <a target="_blank" className={Styles.link} href="http://megalithic.co.uk/">AREMI</a>
 
                 <p>
-                    AREMI provides access to Australian spatial data relevant to the Renewable Energy industry,
-                    sourced from Government, Industry and Research.
-                </p>
-            </div>
-
-            <div className={classNames(DropdownStyles.section, Styles.section)}>
-                <a target="_blank" href="http://nationalmap.gov.au/northernaustralia/">
-                    <img className={Styles.image} src={require('../../wwwroot/images/northernaustralia.jpg')}
-
-                         alt="Northern Australia"/>
-                </a>
-
-                <a target="_blank" className={Styles.link} href="http://nationalmap.gov.au/northernaustralia/">Northern
-                    Australia</a>
-
-                <p>
-                    The Northern Australia NationalMap forms part of the Government's commitment to developing
-                    northern Australia by providing easy access to authoritative and other spatial data on northern
-                    Australia to governments, business and the public.
-
-                    More information on the Government's White Paper on Developing Northern Australia is available
-                    here: <a target="_blank" href="https://northernaustralia.dpmc.gov.au">https://northernaustralia.dpmc.gov.au</a>.
-                </p>
-            </div>
-
-            <div className={classNames(DropdownStyles.section, Styles.section)}>
-                <a target="_blank" href="http://neiiviewer.nicta.com.au">
-                    <img className={Styles.image} src={require('../../wwwroot/images/neii.jpg')}
-                         alt="NEII Viewer"/>
-                </a>
-
-                <a target="_blank" className={Styles.link} href="http://neiiviewer.nicta.com.au">NEII Viewer</a>
-
-                <p>
-                    The National Environmental Information Infrastructure (NEII) is an information platform designed
-                    to improve discovery, access and re-use of nationally significant environmental data. More
-                    information on the NEII is available here: [www.neii.gov.au](http://www.neii.gov.au)
-                </p>
-            </div>
-
-            <div className={classNames(DropdownStyles.section, Styles.section)}>
-                <a target="_blank" href="http://map.aurin.org.au">
-                    <img className={Styles.image} src={require('../../wwwroot/images/aurin-map.jpg')}
-                         alt="AURIN Map"/>
-                </a>
-
-                <a target="_blank" className={Styles.link} href="http://map.aurin.org.au">AURIN Map</a>
-
-                <p>
-                    AURIN Map provides access to datasets on urban infrastructure for urban researchers, policy and
-                    decision makers.
+                    The top destination for Megaliths and Prehistory worldwide. World-wide Ancient Site Database,
+                    Photos and Prehistoric Archaeology News with geolocation.
                 </p>
             </div>
         </DropdownPanel>
