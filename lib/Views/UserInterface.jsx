e1ed6807579055603ba461263b345c0e95dcaa2e
Joerg Roth <info@leylines.ch>
2017-07-10 10:20:55 +0200

    update UserInterface

--- lib/Views/UserInterface.jsx
+++ lib/Views/UserInterface.jsx
@@ -5,7 +5,7 @@ import version from '../../version';
 import StandardUserInterface from 'leylinesjs/lib/ReactViews/StandardUserInterface/StandardUserInterface.jsx';
 import MenuItem from 'leylinesjs/lib/ReactViews/StandardUserInterface/customizable/MenuItem';
 import RelatedMaps from './RelatedMaps';
-import { Menu, ExperimentalMenu } from 'leylinesjs/lib/ReactViews/StandardUserInterface/customizable/Groups';
+import { Menu, Nav, ExperimentalMenu } from 'leylinesjs/lib/ReactViews/StandardUserInterface/customizable/Groups';
 import SplitPoint from 'terriajs/lib/ReactViews/SplitPoint';
 import MeasureTool from 'leylinesjs/lib/ReactViews/Map/Navigation/MeasureTool';
 import AugmentedVirtualityTool from 'leylinesjs/lib/ReactViews/Map/Navigation/AugmentedVirtualityTool';
@@ -30,17 +30,14 @@ export default function UserInterface(props) {
                 <RelatedMaps viewState={props.viewState} />
                 <MenuItem caption="About" href="http://www.leylines.ch/index.html#about" key="about-link"/>
             </Menu>
-<<<<<<< HEAD
             <Nav>
                 <MeasureTool terria={props.viewState.terria} key="measure-tool"/>
             </Nav>
-=======
             <ExperimentalMenu>
                 <If condition={isBrowserSupportedAV()}>
                     <SplitPoint loadComponent={loadAugmentedVirtuality} viewState={props.viewState} terria={props.viewState.terria} experimentalWarning={true}/>
                 </If>
             </ExperimentalMenu>
->>>>>>> upstream/master
         </StandardUserInterface>
     );
 }
